d1a2c23d5bdeb436bd137fc50afb06b21394e620
Paul Furiani <pf@furiani.net>
2016-10-05 14:01:05 -0700

    Adding postinstall script to create resources skeleton & gulpfile

---
+++ postinstall.js
@@ -0,0 +1,20 @@
+var ncp = require('ncp').ncp;
+
+var source = "resources";
+var destination = "../../resources";
+var options = { "clobber": false };
+ncp(source, destination, options, function (err) {
+	if (err) {
+		return console.error(err);
+	}
+ 	console.log("app/resources created successfully!\n");
+});
+
+source = "gulpfile.example.js";
+destination = "../../gulpfile.js";
+ncp(source, destination, options, function (err) {
+    if (err) {
+        return console.error(err);
+    }
+    console.log("app/gulpfile.js created successfully!\n");
+});
