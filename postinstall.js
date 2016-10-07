dabc22e492bbdbfb6c09786f6d042b316ebe2147
Paul Furiani <pf@furiani.net>
2016-10-06 20:14:12 -0700

    Updating postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -7,7 +7,7 @@ ncp(source, destination, options, function (err) {
 	if (err) {
 		return console.error(err);
 	}
- 	console.log("app/resources created successfully!\n");
+ 	console.log("app/resources copied (noclobber) successfully!\n");
 });
 
 source = "elixir.example.json";
@@ -16,7 +16,7 @@ ncp(source, destination, options, function (err) {
     if (err) {
         return console.error(err);
     }
-    console.log("app/elixir.json created successfully!\n");
+    console.log("app/elixir.json copied (noclobber) successfully!\n");
 });
 
 source = "gulpfile.example.js";
@@ -25,5 +25,9 @@ ncp(source, destination, options, function (err) {
     if (err) {
         return console.error(err);
     }
-    console.log("app/gulpfile.js created successfully!\n");
+    console.log("app/gulpfile.js copied (noclobber) successfully!\n");
 });
+
+console.log("Thank you for using CakePHP Elixir!\n");
+console.log("If using the version extension, don't forget to install the CakePHP Version Helper!\n");
+console.log("https://github.com/pfuri/cakephp-elixir-helper\n");
